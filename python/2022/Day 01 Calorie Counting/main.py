d907ae6b8c92623bf0d79b815d064c1c461d0b75
FirinKinuo <deals@fkinuo.ru>
2022-12-01 11:20:15 +0300

    2022: Day 01 | Calorie Counting

---
+++ python/2022/Day 01 Calorie Counting/main.py
@@ -0,0 +1,22 @@
+from python import SolvingBase
+
+
+class Solving(SolvingBase):
+    def __init__(self, *args, **kwargs):
+        super(Solving, self).__init__(*args, **kwargs)
+
+        with open(self.test_case, "r", encoding="utf-8") as input_file:
+            self.elfs: list[int] = [sum(map(int, calories.split('\n')))
+                                    for calories in "".join(input_file.readlines())[:-1].split("\n\n")]
+
+    def first_problem(self):
+        return max(self.elfs)
+
+    def second_problem(self):
+        return sum(sorted(self.elfs, reverse=True)[:3])
+
+
+if __name__ == "__main__":
+    solve = Solving(test_case=False)
+
+    print(f"First Problem: {solve.first_problem()}\nSecond Problem: {solve.second_problem()}")
